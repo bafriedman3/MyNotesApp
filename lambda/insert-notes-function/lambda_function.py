f850c65581aa6868fa31abd0f48ded11836d43df
Boris Friedman <bafriedman3@yahoo.com>
2025-12-13 20:07:04 -0500

    adding lambda-function

---
+++ lambda/insert-notes-function/lambda_function.py
@@ -0,0 +1,35 @@
+import json
+import boto3
+
+# This function updates the notes with the new content
+# updating the corresponding note in the dynamodb table
+
+def lambda_handler(event, context):
+    print(event)
+    if isinstance(event, str):
+        event = json.loads(event)
+    user_id = event['userId']
+    note_id = int(event['noteId'])
+    note_content = event['content']
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('UserNotes')
+
+    try:
+        item = {
+            'userId': user_id,
+            'noteId': note_id,
+            'content': note_content
+        }
+        response = table.put_item(
+            Item=item
+        )
+        
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'noteId': str(note_id), 'message': 'Note created successfully'})
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(str(e))
+        }
